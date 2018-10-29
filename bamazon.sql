a5a7cbab6ba226c4b12437057939730c6b3722a6
Keo <kthorng@gmail.com>
2018-10-28 17:12:06 -0700

    coded

--- bamazon.sql
+++ bamazon.sql
@@ -6,39 +6,39 @@ USE bamazonDB;
 
 CREATE TABLE products (
   item_id INT NOT NULL AUTO_INCREMENT,
-  product_name VARCHAR(45) NOT NULL,
-  department_name VARCHAR(45) NOT NULL,
+  product_name VARCHAR(100) NOT NULL,
+  department_name VARCHAR(100) NOT NULL,
   price DECIMAL(10,2) NOT NULL,
   stock_quantity INT NOT NULL,
   PRIMARY KEY (item_id)
 );
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("", "", 2.50, 100);
+VALUES ("Play Station 4", "Video Games", 349.99, 300);
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("", "", 2.50, 100);
+VALUES ("Red Dead Redemption 2", "Video Games", 49.99, 100);
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("", "", 2.50, 100);
+VALUES ("Xbox One", "Video Games", 290.97, 200);
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("", "", 2.50, 100);
+VALUES ("Call of Duty: Black Ops 4", "Video Games", 58.97, 9);
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("", "", 2.50, 100);
+VALUES ("Sony 65-inch 4K", "Electronics", 1798.00, 5);
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("", "", 2.50, 100);
+VALUES ("Cannon EOS 5D Mark IV DSLR", "Electronics", 3099.99, 10);
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("", "", 2.50, 100);
+VALUES ("Bose QuietComfort 35 Wireless", "Electronics", 349.00, 50);
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("", "", 2.50, 100);
+VALUES ("Instant Pot 6QT", "Home & Kitchen", 79.99, 0);
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("", "", 2.50, 100);
+VALUES ("KitchenAid 5QT", "Home & Kitchen", 300.00, 6);
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("", "", 2.50, 100);
+VALUES ("OmniBlend V", "Home & Kitchen", 234.99, 12);
