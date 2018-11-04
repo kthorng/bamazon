f1c56f84e6ea079506a2ec5510870904b994c06c
Keo <kthorng@gmail.com>
2018-11-04 11:12:32 -0800

    no pw

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -8,7 +8,7 @@ var Table = require('cli-table');
 var connection = mysql.createConnection({
     host: '',
     user: 'root',
-    password: 'ankorwat',
+    password: '',
     database: 'bamazonDB'
 });
 
